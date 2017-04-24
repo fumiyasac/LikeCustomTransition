d553e54f99b82716aa6b9bf1900fc499123e4573
Fumiya Sakai <just1factory@gmail.com>
2017-04-24 16:11:09 +0900

    [More] Setting Fix

---
+++ src/components/common/Intro.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, Button, Text } from 'native-base';
+
+export default class Intro extends Component {
+  render() {
+    return (
+      <Container>
+        <Button>
+          <Text>
+            Button
+          </Text>
+        </Button>
+      </Container>
+    );
+  }
+}
